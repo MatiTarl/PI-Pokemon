eb3d4de9a46b74f4c36117a9d6a19cc365ea4a65
MatiTar <Matiastari@outlook.com.ar>
2023-07-24 20:45:50 -0300

    todas las rutas completadas

--- api/src/routes/pokemonRouter.js
+++ api/src/routes/pokemonRouter.js
@@ -1,18 +1,73 @@
 const { Router } = require('express');
-const pokemon = Router();
-const getPokemons = require("../handlers/getPokemons");
-const getPokemonsById = require("../handlers/getPokemonsById");
+const { Pokemon, Type } = require("../db");
+const pokemons = Router();
+const getPokemonByName = require("../handlers/getPokemonsByName");
+const getPokemonById = require("../handlers/getPokemonsById");
+const getPokemon = require("../handlers/getPokemones");
+const URL = "https://pokeapi.co/api/v2/pokemon";
 
+ pokemons.get("/", async (req, res) => {
+    try {
+        const { name } = req.query;
+        const backPokemon = await Pokemon.findAll();
+        if (name) {
+            return getPokemonByName(req, res, URL);
+        } else {
+            const apiPokemons = await getPokemon(req, res, URL);
+            if(backPokemon){
+            const dataCombined = apiPokemons.concat(backPokemon);
+            return res.send(dataCombined);
+            }
+            return res.status(200).send(apiPokemons)
+        }
+    } catch (error) {
+        res.status(410).send({ "error": error.message });
+    }
+});
 
+ pokemons.get("/:id", async (req, res) => {
+    try {
+    const {id} = req.params;
+    const pokemonByDb = await Pokemon.findOne({where: {id: id}});
+    if(pokemonByDb.length === 0){
+        return getPokemonById(req, res, URL);
+    }
+    return res.status(200).send(pokemonByDb);
+    } catch (error) {
+    return res.status(400).send({"error": error.message});
+    }
+    
+});
 
-pokemon.get("/", (req, res) => {
-    const url = "https://pokeapi.co/api/v2/pokemon";
-    getPokemons(req, res, url);
-})
+ pokemons.post("/", async (req, res) => {
+    try {
+        const { name, imagen, vida, ataque, defensa, type1, type2 } = req.body;
+        if(!name || !imagen || !vida || !ataque || !defensa) {   
+         return res.status(400).send("faltan datos por completar");}
 
-pokemon.get("/:id", (req, res) => {
-    getPokemonsById(req, res);
-})
- 
+         const pokemon = await Pokemon.create({
+            name,
+            imagen,
+            vida,
+            ataque,
+            defensa,
+         });
 
-module.exports = pokemon;
+         if(type1){  // esto es para vincular la tablas con el pokemon creado
+         const type1Found = await Type.findOne({where: {name: type1}})
+         await pokemon.addType(type1Found)
+         } 
+         if(type2){
+         const type2Found = await Type.findOne({where: {name: type2}})
+         await pokemon.addType(type2Found)
+         }
+         
+        return res.status(200).send("pokemon creado correctamente");
+    } catch (error) {
+        return res.status(400).send({"error": error.message});
+
+    }
+    
+ })
+
+ module.exports = pokemons;
--- api/src/routes/typeRouter.js
+++ api/src/routes/typeRouter.js
@@ -1,8 +1,27 @@
 const { Router } = require('express');
+const getType = require("../handlers/getTypes");
+const URL = "https://pokeapi.co/api/v2/type"
+const {Type} = require("../db");
 const type = Router();
 
 
+type.get("/", async (req, res) => {
+     try {
+          const apiDataBase = await Type.findAll()
+          if(apiDataBase.length === 0){
+            const apiData = await getType(req, res, URL);
+          for (const item of apiData) {
+              const {name} =  item; 
+              await Type.create({name})
+         }  
+         return res.status(200).send("Los types fueron cargados correctamente")
+          }
+         return res.status(200).send(apiDataBase);
 
+     } catch (error) {
+        res.status(200).send({"error": error.message})
+     }
+})
 
 
 module.exports = type;
